6270758aa830695b01470f707f2801cab980f02c
Holquaria <jholker4@gmail.com>
2023-02-25 16:49:37 +0000

    add styling to single pokemon page

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,12 +2,11 @@ import Link from "next/link";
 import { capitaliseFirstLetter } from "@/utils/capitalise";
 
 const PokemonCard = ({ poke }) => {
+    console.log(poke)
   return (
     <div className="w-11/12 m-auto">
       <Link href={`/pokemon/${poke.id}`}>
-        <div className="w-full flex flex-row items-center justify-center hover:bg-red-400">
-          <p>{poke.id}</p>
-          <h3 className="text-3xl">{capitaliseFirstLetter(poke.name)}</h3>
+        <div className="w-full flex flex-row items-center justify-center">
           {poke.sprites.front_default ? (
             <img
               className=""
@@ -17,6 +16,16 @@ const PokemonCard = ({ poke }) => {
           ) : (
             <p>No image available</p>
           )}
+          <p className="self-center p-5">{poke.id}</p>
+          <h3 className="self-center text-3xl p-5">
+            {capitaliseFirstLetter(poke.name)}
+          </h3>
+          <ul className="self-center">
+          {poke.types.map((type) => {
+            const typeName = type.type.name
+            return <li key={type.type.name}>{capitaliseFirstLetter(typeName)}</li>
+          })}
+          </ul>
         </div>
       </Link>
     </div>
--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -1,39 +1,70 @@
-import { useRouter } from 'next/router'
-import { capitaliseFirstLetter } from '@/utils/capitalise'
-import Link from 'next/link'
+import { useRouter } from "next/router";
+import { capitaliseFirstLetter } from "@/utils/capitalise";
+import Link from "next/link";
 
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function SinglePokemon({ pokemon }) {
-    const router = useRouter()
-    const { id } = router.query
-    console.log(id)
-    console.log(pokemon)
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(pokemon);
 
-
-
-    return (<>
-    <Head>
-        <title>{pokemon.name} {pokemon.id}</title>
-    </Head>
-        <div>
-            <h2 className='text-3xl'>{capitaliseFirstLetter(pokemon.name)}</h2>
-            {pokemon.sprites.front_default 
-            ? <img src={pokemon.sprites.front_default} alt={`image of ${pokemon.name}`} />
-            : <p>No image available</p>}
-            <Link href={`/pokemon/${parseInt(id) - 1}`}>Previous</Link>
-            <Link href={`/pokemon/${parseInt(id) + 1}`}>Next</Link>
+  return (
+    <>
+      <Head>
+        <title>
+          {pokemon.name} {pokemon.id}
+        </title>
+      </Head>
+      <div>
+        <h2 className="text-3xl text-center">{capitaliseFirstLetter(pokemon.name)} - ID: {pokemon.id}</h2>
+        {pokemon.sprites.front_default ? (
+          <img className="flex m-auto"
+            src={pokemon.sprites.front_default}
+            alt={`image of ${pokemon.name}`}
+          />
+        ) : (
+          <p>No image available</p>
+        )}
+        <ul className="m-auto justify-center flex flex-col items-center self-center">
+          {pokemon.types.map((type) => {
+            const typeName = type.type.name;
+            return (
+              <li className={"px-5 w-32"} id={type.type.name} key={type.type.name}>{capitaliseFirstLetter(typeName)}</li>
+            );
+          })}
+        </ul>
+        <ul className="flex flex-col items-center">
+          <p>Base Stats</p>
+          <div className="border-2 border-black p-4">
+          {pokemon.stats.map((stat) => {
+            return (
+                <li className="">
+                    <p>{stat.stat.name}: {stat.base_stat}</p>
+                    <svg className="" height='20'>
+                    <rect width={stat.base_stat} height="20" className='fill-green-700 hover:fill-green-900 stroke-green-900 stroke-2' />
+                    </svg>
+                </li>
+            )
+          })}
+          </div>
+        </ul>
+        <div className="flex m-auto justify-center p-4">
+            <Link href={`/pokemon/${parseInt(pokemon.id) - 1}`}><p className="px-4">Previous</p></Link>
+            <Link href={`/pokemon/${parseInt(pokemon.id) + 1}`}><p className="px-4">Next</p></Link>
         </div>
-   </> )
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps({ params }) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
-    const data = await res.json();
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+  const data = await res.json();
 
-    return {
-        props: { pokemon: data },
-    }
+  return {
+    props: { pokemon: data },
+  };
 }
 
 // export async function getStaticProps({ params }) {
